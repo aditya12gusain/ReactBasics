86bd6aa497412df9e95d4ff315b322a4c39bd193
Aditya Gusain <adisandi3112002@gmail.com>
2021-09-21 18:12:55 +0530

    Day 3

---
+++ reactbasics/src/App.js
@@ -0,0 +1,11 @@
+import FirstParagraph from "./Components/FirstParagraph";
+
+function App() {
+  return (
+    <div>
+      <FirstParagraph content="First Paragraph" number="4" />
+      <FirstParagraph content="Second Paragraph" number="5" />
+    </div>
+  );
+}
+export default App;
---
+++ reactbasics/src/Components/FirstParagraph.jsx
@@ -0,0 +1,10 @@
+function FirstParagraph({ number, content }) {
+  return (
+    <>
+      <p>{content}</p>
+      <p>{number}</p>
+    </>
+  );
+}
+
+export default FirstParagraph;
