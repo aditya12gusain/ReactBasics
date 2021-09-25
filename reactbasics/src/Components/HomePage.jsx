98b2dcff51133ba1b2419a567069be1d709ea8ce
Aditya Gusain <adisandi3112002@gmail.com>
2021-09-25 17:55:39 +0530

    Day 7

--- reactbasics/src/Components/HomePage.jsx
+++ reactbasics/src/Components/HomePage.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function HomePage() {
+  const [number, setNumber] = useState(0);
+
+  //ComponentDidMount
+  useEffect(() => {
+    console.log("Component Mounted");
+  }, []);
+
+  //ComponentDidUpdate
+  useEffect(() => {
+    console.log("Number Updated");
+  }, [number]);
+
+  //ComponentDidUnmount
+  useEffect(() => {
+    return () => console.log("Component Unmounted");
+  });
+
   return (
     <>
       <div
@@ -11,6 +28,12 @@ function HomePage() {
         }}
       >
         Welcome to Home Page
+      </div>
+      <div>
+        <h1 style={{ textAlign: "center" }}>{number}</h1>
+        <button onClick={() => setNumber((prev) => prev + 1)}>
+          Click Me to add 1
+        </button>
       </div>
     </>
   );
